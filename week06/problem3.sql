afbd89ce0497d5a9a64eee0697210bd5f0504dd9
ZahraaRagab <alzahraaragab40@gmail.com>
2023-02-19 13:21:30 +0200

    SQLPROBLEMS

---
+++ week06/problem3.sql
@@ -0,0 +1,6 @@
+
+SELECT SUM(City.population)
+FROM Country
+INNER JOIN City 
+ON Country.Code = City.CountryCode
+WHERE Country.Continent='Asia';
---
+++ week06/problem4.sql
@@ -0,0 +1,11 @@
+UPDATE salary
+
+SET
+
+sex = CASE sex
+
+        WHEN 'm' THEN 'f'
+
+        ELSE 'm'
+
+    END;
